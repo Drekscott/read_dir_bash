913ef75ade05036e4a869f901954dfd5e2e0f192
drekscott <drekscott@gmail.com>
2016-09-18 23:08:01 -0400

    Function reads a given directory. Prints out the contents of the directory. If it finds another sub-directory then repeats process.

---
+++ a3.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+#! /bin/sh
+
+# Name:Dreyton Scott;	Class:ITC 510
+# Assignment:3;		Due:9/21/16
+
+# Function:usage(), Gives user insight on how to use script
+
+usage(){
+	echo "Usage: $0 <directory>"
+	exit 1
+}
+
+# Function: Given a directory this function will print out all
+# contents, and if it stumbles upon another directory then we
+# recall this function. This will use a breadth first search
+# approach.
+
+process_directory(){
+	echo "Directory Given: $1"
+	lsdir=`ls -x $1`
+	#check if input is directory
+	if [ -d $1 ]; then
+		echo "$lsdir"
+		for dir in $lsdir; do
+			dtopr=$1/$dir
+			if [ -d $dtopr ]; then
+				process_directory "$dtopr"
+			fi
+		done
+	fi
+	echo "Subprocess finished successfully"
+}
+
+if [ $# -ne 1 ]; then
+	usage
+fi
+process_directory "$1"
+echo "Process finished successfully"
+exit
+
